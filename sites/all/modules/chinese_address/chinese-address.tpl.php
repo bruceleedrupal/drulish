4c9ce055273214f15e7857db3758107b3d8a71a1
Bruce Lee <brucelee.drupal@gmail.com>
2018-12-09 19:40:07 +0800

    backup at 2018-12-09 19:40:01

--- sites/all/modules/chinese_address/chinese-address.tpl.php
+++ sites/all/modules/chinese_address/chinese-address.tpl.php
@@ -4,9 +4,9 @@
  * @file
  */
 foreach($address as $i => $a) {
-  if($a == CHINESE_ADDRESS_NAME_HIDE) {
-    unset($address[$i]);
-  }
+    if($a == CHINESE_ADDRESS_NAME_HIDE) {
+        unset($address[$i]);
+    }
 }
 
 echo implode('', $address);
